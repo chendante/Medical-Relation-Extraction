bdb7e609e8267228b64be4c1d982ce0a00b8b6a9
chendante <danteliuzichen@163.com>
2020-08-04 15:12:18 +0800

    pre

--- preparation/prepare_data.py
+++ preparation/prepare_data.py
@@ -1,13 +1,50 @@
 import json
 from transformers import BertTokenizer
+import codecs
 
-bert_tokenizer = BertTokenizer.from_pretrained("../pretrained_model/")
+bert_tokenizer = BertTokenizer.from_pretrained("../pretrained_model/bert_wwn/")
 
 
 class DataProcess(object):
     def __init__(self, pretrained_path, raw_data_path, is_test=True):
         self.bert_tokenizer = BertTokenizer.from_pretrained(pretrained_path)
+        self.raw_data_path = raw_data_path
+        self.is_test = is_test
+        self.schemas = self.schemas_init()
+        self.train_data = self.train_data_init()
 
+    def schemas_init(self):
+        schemas = []
+        with codecs.open(self.raw_data_path + "train_data.json", encoding='utf-8') as f:
+            while True:
+                line = f.readline()
+                if line == "":
+                    break
+                schema = json.loads(line)
+                schemas.append("_".join([schema["subject_type"], schema["predicate"], schema["object_type"]]))
+        return schemas
+
+    def train_data_init(self):
+        train_data = []
+        with codecs.open(self.raw_data_path + "train_data.json", encoding='utf-8') as f:
+            while True:
+                line = f.readline()
+                if line == "":
+                    break
+                train_data.append(json.loads(line))
+
+        if not self.is_test:
+            with codecs.open(self.raw_data_path + "val_data.json", encoding='utf-8') as f:
+                while True:
+                    line = f.readline()
+                    if line == "":
+                        break
+                    train_data.append(json.loads(line))
+        return train_data
+
+    def process(self):
+        for t_d in self.train_data:
+            
 
 
 def main():
