8a0ca27019ca6c070f69927624ab8750eeb68942
chendante <danteliuzichen@163.com>
2020-08-05 13:54:37 +0800

    '1'

--- run_classifier.py
+++ run_classifier.py
@@ -2,18 +2,131 @@ from transformers import *
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 import torch
+import time
+import datetime
+
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+def format_time(elapsed):
+    '''
+    Takes a time in seconds and returns a string hh:mm:ss
+    '''
+    # 四舍五入到最近的秒
+    elapsed_rounded = int(round((elapsed)))
+
+    # 格式化为 hh:mm:ss
+    return str(datetime.timedelta(seconds=elapsed_rounded))
 
 
 def main():
-    model = BertForSequenceClassification.from_pretrained("./pretrained_model/bert_wwn_ext/", num_labels=53)
-    input_ids = torch.cat(np.load("./processed_data/input_ids.npy"), 0)
-    attention_masks = torch.cat(np.load("./processed_data/attention_masks.npy"))
-    labels = torch.cat(np.load("./processed_data/labels.npy"))
+    model = BertForSequenceClassification.from_pretrained(
+        "./pretrained_model/bert_wwm/",
+        num_labels=53,
+        output_attentions=False,  # 模型是否返回 attentions weights.
+        output_hidden_states=False,  # 模型是否返回所有隐层状态.
+    )
+    input_ids = torch.tensor(torch.from_numpy(np.load("./preparation/processed_data/input_ids.npy")), dtype=torch.long)
+    attention_masks = torch.tensor(torch.from_numpy(np.load("./preparation/processed_data/attention_masks.npy")), dtype=torch.long)
+    labels = torch.tensor(torch.from_numpy(np.load("./preparation/processed_data/labels.npy")), dtype=torch.long)
     dataset = TensorDataset(input_ids, attention_masks, labels)
     model.cuda()
 
     train_dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
+    optimizer = AdamW(model.parameters(),
+                      lr=2e-5,  # args.learning_rate - default is 5e-5
+                      eps=1e-8  # args.adam_epsilon  - default is 1e-8
+                      )
+
+    # 训练 epochs。 BERT 作者建议在 2 和 4 之间，设大了容易过拟合
+    epochs = 4
+
+    # 总的训练样本数
+    total_steps = len(train_dataloader) * epochs
+
+    # 创建学习率调度器
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0,
+                                                num_training_steps=total_steps)
+    total_t0 = time.time()
+
+    for epoch_i in range(0, epochs):
+
+        # ========================================
+        #               Training
+        # ========================================
+
+        print("")
+        print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+        print('Training...')
+
+        # 统计单次 epoch 的训练时间
+        t0 = time.time()
+
+        # 重置每次 epoch 的训练总 loss
+        total_train_loss = 0
+
+        # 将模型设置为训练模式。这里并不是调用训练接口的意思
+        model.train()
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # 训练集小批量迭代
+        for step, batch in enumerate(train_dataloader):
+
+            # 每经过40次迭代，就输出进度信息
+            if step % 40 == 0 and not step == 0:
+                elapsed = format_time(time.time() - t0)
+                print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
+
+            # 准备输入数据，并将其拷贝到 gpu 中
+            b_input_ids = batch[0].to(device)
+            b_input_mask = batch[1].to(device)
+            b_labels = batch[2].to(device)
+
+            # 每次计算梯度前，都需要将梯度清 0，因为 pytorch 的梯度是累加的
+            model.zero_grad()
+
+            # 前向传播
+            # 文档参见:
+            # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
+            # 该函数会根据不同的参数，会返回不同的值。 本例中, 会返回 loss 和 logits -- 模型的预测结果
+            loss, logits = model(b_input_ids,
+                                 token_type_ids=None,
+                                 attention_mask=b_input_mask,
+                                 labels=b_labels)
+
+            # 累加 loss
+            total_train_loss += loss.item()
+
+            # 反向传播
+            loss.backward()
+
+            # 梯度裁剪，避免出现梯度爆炸情况
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            # 更新参数
+            optimizer.step()
+
+            # 更新学习率
+            scheduler.step()
+
+        # 平均训练误差
+        avg_train_loss = total_train_loss / len(train_dataloader)
+
+        # 单次 epoch 的训练时长
+        training_time = format_time(time.time() - t0)
+
+        print("")
+        print("  Average training loss: {0:.2f}".format(avg_train_loss))
+        print("  Training epcoh took: {:}".format(training_time))
+
+    print("")
+    print("Training complete!")
+    print("Total training took {:} (h:mm:ss)".format(format_time(time.time() - total_t0)))
 
 
 if __name__ == "__main__":
